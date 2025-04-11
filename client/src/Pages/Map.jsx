b6ba9bbf9fdd583e71d0c214c122a9516586a021
alok <alok.kr300@gmail.com>
2025-04-11 20:33:27 +0530

    Created Map page

---
+++ client/src/Pages/Map.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+export default function Map() {
+  return (
+    <div className="flex h-screen w-screen">
+      <div className="w-[25%] bg-black/80 text-white flex items-center justify-center"></div>
+      <div className="w-[75%]">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
+}
